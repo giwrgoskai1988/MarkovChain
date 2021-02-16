2f865b327c7dc8d031cb1eec7e6b401c3a384360
giwrgoskai1988 <giwrgoskai1988@gmail.com>
2021-02-16 19:44:56 +0200

    First Attempt

---
+++ MarkovChains/Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MarkovChains
+{
+    class Menu
+    {
+        public Dictionary<string , List<string>> wordPairs = new Dictionary<string, List<string>>();
+        public string PathInput { get; private set; }
+
+
+        public Menu()
+        {
+            Run();
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Please enter a proper folder path to train me or press Enter to quit: ");
+
+            PathInput =  @"C:\Users\Α\source\repos"; ;
+
+            while (PathInput != "")
+            {
+                if (PathExists())
+                {
+                    Train();
+                }
+                else
+                {
+                    Console.WriteLine("Path is wrong! Enter proper path or press Enter to quit! : ");
+                }
+
+                PathInput = Console.ReadLine();
+
+            }
+        }
+
+
+        bool PathExists()
+        {
+            return Directory.Exists(PathInput);
+        }
+
+
+        void Train()
+        {
+            string[] files = Directory.GetFiles(PathInput);
+
+            foreach (var file in files)
+            {
+                StreamReader sr = new StreamReader(new BufferedStream(new FileStream(file, FileMode.Open)));
+                string line = "";
+                while((line = sr.ReadLine())!= null)
+                {
+                    string[] words = line.Split(' ');
+
+                    for (int i = 0; i < words.Length - 1; i++)
+                    {
+                        if(wordPairs.ContainsKey(words[i].ToLower()))
+                        {
+                            wordPairs[words[i].ToLower()].Add(words[i + 1].ToLower());
+                        }
+                        else
+                        {
+                            wordPairs.Add(words[i].ToLower(), new List<string> { words[i + 1].ToLower() });
+                        }
+                    }                 
+                }
+                sr.Close();
+            }
+        }
+
+    }
+}
---
+++ MarkovChains/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MarkovChains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+          string a =  @"C:\Users\Α\source\repos\MarkovChains\g.txt";
+
+
+            string b = @"C:\Users\Α\source\repos";
+            Menu bb = new Menu();
+    }
+    }
+}
